f24a9e899b558019547df287073661e9a98aeb28
hulkcao <hulkcao@tencent.com>
2022-08-18 00:08:23 +0800

    add addtwo sum

--- arrary/6.twoSum.go
+++ arrary/6.twoSum.go
@@ -2,9 +2,8 @@ package arrary
 
 //https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
 
-func TwoSum(numbers []int, target int) []int {
-	leftIndex := 0
-	rightIndex := len(numbers) - 1
+func twoSum(numbers []int, target int) []int {
+	leftIndex, rightIndex := 0, len(numbers)-1
 	for leftIndex < rightIndex {
 		if numbers[leftIndex]+numbers[rightIndex] == target {
 			return []int{leftIndex + 1, rightIndex + 1}
@@ -16,5 +15,24 @@ func TwoSum(numbers []int, target int) []int {
 		}
 	}
 
-	return []int{}
+	return []int{-1, -1}
+}
+
+//二分法
+func twoSum2(numbers []int, target int) []int {
+	for i := 0; i < len(numbers); i++ {
+		n := target - numbers[i]
+		low, high := i+1, len(numbers)-1
+		for low <= high {
+			mid := low + (high-low)/2
+			if n == numbers[mid] {
+				return []int{i + 1, mid + 1}
+			} else if numbers[mid] < n {
+				low = mid + 1
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+	return []int{-1, -1}
 }
--- arrary/6.twoSum_test.go
+++
@@ -1,8 +0,0 @@
-package arrary
-
-import "testing"
-
-func TestTwoSum(t *testing.T) {
-	numbers := []int{2, 7, 11, 15}
-	TwoSum(numbers, 9)
-}
